907b52d9a9d949ffdbd8867667117e8fcf652859
David Deharbe <deharbe@gmail.com>
2015-06-02 17:15:57 -0300

    filter-unknown.py now also filters out all benchmarks with id listed in file remove.txt (one id per line).

--- StarExec-Utilities/filter-unknown.py
+++ StarExec-Utilities/filter-unknown.py
@@ -9,23 +9,28 @@
 # David Deharbe - Universidade Federal do Rio Grande do Norte (c) 2015
 
 import optparse
+import os
 import sys
 import xml.etree.ElementTree as ET
 
 removed = 0
 kept = 0
+removable = []
 
 def remove_unknown_rec(node):
-    global removed, kept
+    global removed, kept, removable
     for bench in node.findall('Benchmark'):
-        unknown = False
-        for attribute in bench.findall('Attribute'):
-            if attribute.attrib['name'] == 'status' and attribute.attrib['value'] == 'unknown':
-                unknown = True
-        if unknown:
+	remove = False
+	if int(bench.attrib['id']) in removable:
+	    remove = True
+	else:
+            for attribute in bench.findall('Attribute'):
+                if attribute.attrib['name'] == 'status' and attribute.attrib['value'] == 'unknown':
+                    remove = True
+        if remove:
             removed = removed + 1
             node.remove(bench)
-            sys.stderr.write(bench.attrib['name'] + ' removed\n')
+            sys.stderr.write(bench.attrib['name'] +'(' + bench.attrib['id'] + ') removed\n')
         else:
             kept = kept + 1
     for child in node:
@@ -33,11 +38,16 @@ def remove_unknown_rec(node):
 
 
 def main():
+    global removed, kept, removable
     p = optparse.OptionParser(description='removes benchmarks with status unknown from StarExec space XML',
                               prog='filter-unknown',
                               usage='%prog < input > output')
     options, arguments = p.parse_args()
     if len(arguments) == 0:
+	if os.path.exists('remove.txt'):
+	    removable = set([ int(line) for line in open('remove.txt').readlines() ])
+        else:
+            removable = set([])
         xml = ET.parse(sys.stdin)
         root = xml.getroot()
         remove_unknown_rec(root)
